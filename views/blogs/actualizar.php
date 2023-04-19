5f0dca5ca386a8765d21c9a6a11145f31f8f8032
juanirco <58012197+juanirco@users.noreply.github.com>
2023-04-19 06:52:45 -0600

    Revert "Delete views/blogs directory"
    This reverts commit 6e7295bd659c9bf8cf3c24b4163ace9361988a06.

---
+++ views/blogs/actualizar.php
@@ -0,0 +1,22 @@
+<main class="contenedor seccion">
+        <h1>Actualizar Blog</h1>
+
+        <a href="/admin" class="boton boton-amarillo">Volver</a>
+
+        <?php 
+        if(!empty ($errores)) {?>
+        <?php foreach ($errores as $error): ?> 
+                <div class="alerta error">
+                <?php echo $error; ?>
+            </div>
+        <?php endforeach;
+        }            
+        
+        ?>
+
+        <form class="formulario" method="POST" enctype="multipart/form-data">
+        <?php include __DIR__ . '/formulario.php'; ?>
+
+        <input type="submit" value="Actualizar Blog" class="boton boton-verde contener-boton">
+        </form>
+</main>
