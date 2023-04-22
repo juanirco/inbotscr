c3d996d4f10c1738807b2742162f351c742683e1
juanirco <juanirco@hotmail.com>
2023-04-21 18:11:23 -0600

    Inbotscr Fixed

--- Router.php
+++ Router.php
@@ -18,20 +18,21 @@ class Router {
     public function comprobarRutas() {
 
         session_start();
-        $auth = $_SESSION['login'] ?? null;
-
-        // Arreglo de rutas protegidas
-        $rutas_protegidas = ['/admin', '/demos/crear', '/demos/actualizar', '/demos/eliminar', '/administradores/crear', '/administradores/actualizar', '/administradores/eliminar', '/blogs/crear', '/blogs/actualizar', '/blogs/eliminar'];
 
         $urlActual = $_SERVER['REQUEST_URI'] === '' ? '/' : $_SERVER['REQUEST_URI'];
         $metodo = $_SERVER['REQUEST_METHOD'];
-        // debug($metodo);
 
         if($metodo === 'GET') {
             $fn = $this->rutasGET[$urlActual] ?? null;
         }else {
             $fn = $this->rutasPOST[$urlActual] ?? null;
         }
+
+        // debug($metodo);
+        $auth = $_SESSION['login'] ?? null;
+
+        // Arreglo de rutas protegidas
+        $rutas_protegidas = ['/admin', '/demos/crear', '/demos/actualizar', '/demos/eliminar', '/administradores/crear', '/administradores/actualizar', '/administradores/eliminar', '/blogs/crear', '/blogs/actualizar', '/blogs/eliminar'];
 
         if(in_array($urlActual, $rutas_protegidas) && !$auth) {
             header('Location: /home');
--- includes/app.php
+++ includes/app.php
@@ -5,7 +5,7 @@ $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->safeLoad();
 
 require 'funciones.php';
-require 'config/database.php';
+require 'database.php';
 
 
 //Conectarse a la base de datos
